b802387e392a3c4aafa761984ec4ee948c408fab
Vanilla M <bidisaaaa@gmail.com>
2025-03-16 19:28:04 +0530

    changes in core js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div>
-      <h1>YourMoji - Mood Tracker</h1>
+      <Header />
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Header() {
+  return <h1>YourMoji - Mood Tracker</h1>;
+}
+
+export default Header;
